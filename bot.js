26141c36873c54d5683c222cf382aad19dac1677
BCDRA <43544739+BCDRA@users.noreply.github.com>
2018-09-24 18:46:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,66 @@
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '$'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+client.user.setActivity("Eagle bot | helpme",{type: 'WATCHING'});
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+
+
+
+
+client.on('message',function(message) {
+  if (message.author.bot) return;
+var prefix = "!";
+                  if(!message.channel.guild) return;
+
+                    if (message.content === prefix + "inv") {
+ const embed = new Discord.RichEmbed()
+
+    .setDescription(`**Members info ✨
+💚 online:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
+❤  dnd:       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
+💛  idle:     ${message.guild.members.filter(m=>m.presence.status == 'idle').size}
+💠   membersCount:  ${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size}
+💡 bots: ${message.guild.members.filter(m=>m.user.bot).size} **`)
+         message.channel.send({embed});
+
+    }
+      });
+ 
+
+
+
+
+
+
+client.login("NDg3MTY5OTM2
+5MDAwOTYw.DnOadg.wUL_r7HpDUAAOF7xAFS5nkugZFM");
+ 
